bb45a2f1536ae3d71e3d698407e1b37e625afe77
nchloride <clankz39@gmail.com>
2022-01-15 22:20:23 +0800

    modify: footer year

--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -3,7 +3,7 @@ import "./footer.css"
 const Footer = () => {
     return (
         <div className="footer">
-            <h1>Developed by Carlo Lopez © 2021</h1>
+            <h1>Developed by Carlo Lopez © 2022</h1>
             {/* <div className="links">
                 <Link to="/Projects" className="footer__link">Projects</Link>
                 <Link to="/Contacts" className="footer__link">Contacts</Link>
